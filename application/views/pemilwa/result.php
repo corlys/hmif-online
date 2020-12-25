6a6400d6c2265364d3dbc4328150e1a07ac7486f
corlys <duscae047@gmail.com>
2020-12-25 19:39:45 +0700

    add stuff

---
+++ application/views/pemilwa/result.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/resultpemilwa.css">
+    <title>PEMILWA 2020</title>
+</head>
+
+<body>
+    <header></header>
+    <main>
+        <div class="candidate-item">
+            <div class="candidate-img"></div>
+            <div class="candidate-votes"></div>
+        </div>
+        <div class="candidate-item">
+            <div class="candidate-img"></div>
+            <div class="candidate-votes"></div>
+        </div>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
